939ec8520d2fcfe8a75e693ad407d0e0c153ea7f
Ashok Goli <ashok2ashok@gmail.com>
2026-07-19 09:09:40 -0700

    AutoFix CI: Address build failures (attempt 1)
    Auto-generated by AutoFix Agent to resolve CI check failures.

--- src/main/java/org/springframework/samples/petclinic/system/CrashController.java
+++ src/main/java/org/springframework/samples/petclinic/system/CrashController.java
@@ -1,8 +1,8 @@
-package org.springframework.samples.petclinic.system;
-
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+/*
+ * Copyright 2012-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
 
 @Controller
 @Profile("!production")
@@ -17,17 +17,21 @@ class CrashController {
  */
 package org.springframework.samples.petclinic.system;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * Controller used to showcase what happens when an exception is thrown
- *
- * @author Michael Isvy
- * <p/>
- * Also see how a view that resolves to "error" has been added ("error.html").
- */
 @Controller
+@Profile("!production")
+class CrashController {
+
+	@GetMapping("/oups")
+	public String triggerException() {
+		throw new RuntimeException(
+				"Expected: controller used to showcase what happens when an exception is thrown");
+	}
+
+}
 class CrashController {
 
 	@GetMapping("/oups")
